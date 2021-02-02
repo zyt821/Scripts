91de9f7ff793283b5aebfc32720fd6a07cfbed7c
zyt821 <33255990+zyt821@users.noreply.github.com>
2021-02-02 19:53:56 +0800

    Update dyjsb.js

--- dyjsb.js
+++ dyjsb.js
@@ -207,10 +207,14 @@ if (!signheaderArr[0]) {
       readkey = readkeyArr[i];
       $.index = i + 1;
       console.log(`\n开始【抖音极速版${$.index}】`)
-      console.log(`${signheader}`)
-      console.log(`${signcookier}`)
-      console.log(`${readheader}`)
-      console.log(`${readkey}`)
+      console.log(signheader)
+      console.log(`\n`)
+      console.log(signcookier)
+      console.log(`\n`)
+      console.log(readheader)
+      console.log(`\n`)
+      console.log(readkey)
+      console.log(`\n`)
       //await invite()
       await sign_in()
       //await step_submit()
